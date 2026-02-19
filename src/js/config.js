339dc97a3edd8cb792483f18b08168cc2c9d7307
subhaLakshmi-S <subha14092000@gmail.com>
2026-02-20 00:52:43 +0530

    first commit

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR_KEY>';
+export const KEY = '4d282bc5-6c5b-43ef-8d3d-6fad2144c4ac';
 export const MODAL_CLOSE_SEC = 2.5;
