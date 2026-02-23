41a1ba2d6fcee39d829889b068fed95c74751a80
Subha lakshmi S <74890926+subhaLakshmi-S@users.noreply.github.com>
2026-02-23 14:29:07 +0530

    Update API key in config.js

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '4d282bc5-6c5b-43ef-8d3d-6fad2144c4ac';
+export const KEY = '4cf22a58-5a0c-4318-b51f-643112450667';
 export const MODAL_CLOSE_SEC = 2.5;
