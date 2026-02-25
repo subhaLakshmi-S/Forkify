d00bea143b8459672c355360d9cc50bf48d8f9e5
Subha lakshmi S <74890926+subhaLakshmi-S@users.noreply.github.com>
2026-02-25 11:54:39 +0530

    Delete src/js/config.js

--- src/js/config.js
+++
@@ -1,5 +0,0 @@
-export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
-export const TIMEOUT_SEC = 10;
-export const RES_PER_PAGE = 10;
-export const KEY = '4cf22a58-5a0c-4318-b51f-643112450667';
-export const MODAL_CLOSE_SEC = 2.5;
